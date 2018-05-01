0c0048e53e8ad8fe65b43f08d95599e6255d7cbe
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-04-30 20:22:44 -0700

    lab3: add some communication

--- lab3/client.cc
+++ lab3/client.cc
@@ -22,19 +22,55 @@ void save_message(std::string msg) {
 	hashchain = hash(hashchain + sig);
 	key = hash(key);
 	messages.push_back(msg_encrypted);
-
 }
 
 int main() {
 	zmq::context_t context;
 	zmq::socket_t socket(context, zmq::socket_type::rep);
+	socket.bind("tcp://127.0.0.1:7000");
+
+	zmq_pollitem_t items[2] = {};
+
+	items[0].socket = (void*)socket;
+	items[0].events = ZMQ_POLLIN;
+
+	items[1].fd = 0;
+	items[1].events = ZMQ_POLLIN;
 
 	for (;;) {
+		int r = zmq::poll(items, 2, 0);
+
 		// when a messae arrives,
 		// queue the message
-		//
+		if (items[1].revents & ZMQ_POLLIN) {
+			if (!std::cin) {
+				break;
+			}
+			std::cout << "got a message\n";
+			std::string line;
+			std::getline(std::cin, line);
+			save_message(line);
+		}
+
 		// when the collector arrives,
 		// send a batch of messages
+		if (items[0].revents & ZMQ_POLLIN) {
+			std::cout << "got a collection request\n";
+
+			zmq::message_t ignore;
+			socket.recv(&ignore);
+
+			if (!messages.empty()) {
+				string s = messages.back();
+				zmq::message_t msg(s.size());
+				memmove(msg.data(), s.data(), s.size());
+				socket.send(msg);
+				messages.pop_back();
+			} else {
+				zmq::message_t msg;
+				socket.send(msg);
+			}
+		}
 	}
 
 	return 0;
--- lab3/server.cc
+++ lab3/server.cc
@@ -1,5 +1,21 @@
+#include <unistd.h>
 #include <iostream>
+#include "zmq.hpp"
 
 int main() {
+	zmq::context_t context;
+	zmq::socket_t socket(context, zmq::socket_type::req);
+	socket.connect("tcp://localhost:7000");
+
+	for (;;) {
+		// send a request to the client
+		zmq::message_t msg;
+		socket.send(msg);
+		socket.recv(&msg);
+		auto s = std::string(reinterpret_cast<char*>(msg.data()), msg.size());
+		std::cout << s << "\n";
+		sleep(1);
+	}
+
 	return 0;
 }
