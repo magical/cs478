87b8d691f10f614419b321ab223dac225e7157ab
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-05-14 23:32:06 -0700

    lab3: implement huffman table encoding & decoding

--- lab3/compress.cc
+++ lab3/compress.cc
@@ -24,21 +24,64 @@ public:
 typedef std::map<char, Node*> histogram_t;
 typedef std::map<char, string> codemap_t;
 
-Node* decode_tree(string msg);
 string decode_message(Node* tree, string msg);
 
+std::vector<Node*> read_node_list(const string &msg);
+string encode_node_list(const std::vector<Node*> &nodes);
+Node* build_tree(std::vector<Node*> nodes);
 codemap_t convert_tree(Node* tree);
 void walk_node(Node* node, string prefix, codemap_t &codes);
 void writebits(string& s, const string &bits);
 
+unsigned char uch(char c) { return static_cast<unsigned char>(c); }
+
 string decompress(const string &msg) {
 	size_t tree_length = get64(msg, 0);
-	Node *tree = decode_tree(msg.substr(8, tree_length));
+	auto nodes = read_node_list(msg.substr(8, tree_length));
+	Node *tree = build_tree(nodes);
 	return decode_message(tree, msg.substr(8 + tree_length));
 }
 
-Node* decode_tree(string msg) {
-	return NULL;
+std::vector<Node*> read_node_list(const string &msg) {
+	std::vector<Node*> nodes;
+	int state = 0;
+	// the tree is stored as a list of char, count pairs
+	// which provide a histogram of the original input.
+	// from these we can reconstruct the tree.
+	// the count is stored as a variable-length encoding
+	// 1xxxxxxxx          =          0xxxxxxx
+	// 0xxxxxxxx 1yyyyyyy = 00xxxxxx xyyyyyyy
+	// etc
+	int value = 0;
+	int count = 0;
+
+	for (char c : msg) {
+		if (state == 0) {
+			value = uch(c);
+			state++;
+		} else if (state == 1) {
+			int x = uch(c);
+			count = (count << 7) + (x & 0x7f);
+			if (x >> 7) {
+				state = 0;
+
+				Node* n = new Node();
+				n->leaf = true;
+				n->value = value;
+				n->count = count;
+				nodes.push_back(n);
+			}
+		}
+	}
+
+	// add implicit eof symbol
+	Node *eof = new Node();
+	eof->leaf = true;
+	eof->eof = true;
+	eof->count = 1;
+	nodes.push_back(eof);
+
+	return nodes;
 }
 
 string decode_message(Node* tree, string msg) {
@@ -46,7 +89,7 @@ string decode_message(Node* tree, string msg) {
 	string output = "";
 	for (char c : msg) {
 		for (int i = 0; i < 8; i++) {
-			unsigned bit = (((unsigned char)c)>>i) & 1U;
+			unsigned bit = (uch(c)>>i) & 1U;
 			if (bit) {
 				cur = cur->left;
 			} else {
@@ -85,6 +128,8 @@ string compress(const string &msg) {
 		hist[c]->count++;
 	}
 
+	string encoded_nodes = encode_node_list(nodes);
+
 	// add eof symbol
 	Node *eof = new Node();
 	eof->leaf = true;
@@ -93,6 +138,39 @@ string compress(const string &msg) {
 	nodes.push_back(eof);
 
 	// build tree
+	Node *tree = build_tree(nodes);
+
+	// convert tree to codes
+	codemap_t codes = convert_tree(tree);
+
+	// compress
+	string output;
+	output.resize(8);
+	put64(output, encoded_nodes.size());
+	output += encoded_nodes;
+	for (char c : msg) {
+		writebits(output, codes.at(c));
+	}
+	writebits(output, codes.at('\0'));
+	return output;
+}
+
+string encode_node_list(const std::vector<Node*> &nodes) {
+	string output;
+
+	for (const Node* n : nodes) {
+		output.push_back(n->value);
+		auto count = n->count;
+		while (count > 0x7F) {
+			output.push_back(count & 0x7F);
+		}
+		output.push_back(count | 0x80);
+	}
+	return output;
+}
+
+// build a tree from a node list, destrying it in the process
+Node* build_tree(std::vector<Node*> nodes) {
 	std::make_heap(nodes.begin(), nodes.end(), compare_node_count);
 	while (nodes.size() > 1) {
 		// pop the two lowest nodes to form a new node
@@ -112,20 +190,8 @@ string compress(const string &msg) {
 		std::push_heap(nodes.begin(), nodes.end(), compare_node_count);
 	}
 
-	Node *tree = nodes.at(0);
-
-	// convert tree to codes
-	codemap_t codes = convert_tree(tree);;
-
-	// compress
-	string output;
-	for (char c : msg) {
-		writebits(output, codes.at(c));
-	}
-	writebits(output, codes.at('\0'));
-	return output;
+	return nodes.at(0);
 }
-
 
 codemap_t convert_tree(Node* tree) {
 	codemap_t codes;
