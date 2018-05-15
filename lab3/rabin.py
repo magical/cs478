3538e07c50bca5a36a334ce92aaba4bd969ddb90
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-05-14 17:44:43 -0700

    rabin: implement gaussian solver

--- lab3/rabin.py
+++ lab3/rabin.py
@@ -14,31 +14,62 @@ def dispersal(V, l, t):
     # break message into N/t chunks, each with t elements
     # for each chunk, perform dispersal
 
-    M = numpy.zeros((t, l))
-    for i in range(t):
-        for j in range(l):
-            M[i,j] = (i+1)**j
+    M = vandermonde(t, l)
 
     return M.dot(V) % P
 
 def recover(U, l, t):
-    M = numpy.zeros((t, l))
-
-    for i in range(t):
-        for j in range(l):
-            M[i,j] = (i+1)**j
+    M = vandermonde(t, l)
 
     M = M[l:,] # lose some rows
     U = U[l:]
 
-    det = numpy.linalg.det(M)
-    detinv = pow(int(det), P-2, P)
-    Minv = numpy.round(numpy.linalg.inv(M)*det)*detinv % P
+    V = solve(M, U)
 
-    print(Minv.dot(M) % P)
-
-    V = Minv.dot(U) % P
     return V
+
+def vandermonde(t, l):
+    M = numpy.zeros((t, l), dtype=int)
+    for i in range(t):
+        for j in range(l):
+            M[i,j] = pow(i+1, j, P)
+    return M
+
+def solve(M, U):
+    """gaussian elimination"""
+    n = M.shape[0]
+    M = augment(M, U)
+
+    def cancel(k,i):
+        """cancel row i using row k"""
+        d = (-M[i,k] * inv(M[k,k], P)) % P
+        for j in range(0, n+1):
+            M[i,j] = (M[i,j] + M[k,j] * d) % P
+        assert M[i,k] == 0
+
+    print(M)
+
+    for k in range(n):
+        for i in range(k+1, n):
+            cancel(k,i)
+
+    for k in reversed(range(n)):
+        for i in reversed(range(0, k)):
+            cancel(k,i)
+
+    print(M)
+
+    V = [M[k,n]*inv(M[k,k], P)%P for k in range(n)]
+
+    return numpy.array(V)
+
+def inv(n, p):
+    """find n^-1 mod p"""
+    return pow(n, p-2, p)
+
+def augment(M, V):
+    """creates an augmented matrix out of a square matrix and a vector"""
+    return numpy.append(M, numpy.transpose([V]), axis=1)
 
 
 print(dispersal([1, 2, 3, 4], 4, 8))
