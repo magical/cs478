1b852a9ae58c1b3984cea25b3fee12a761f6817c
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-05-15 13:40:57 -0700

    random-rs: it works!

--- random-rs/src/main.rs
+++ random-rs/src/main.rs
@@ -1,6 +1,7 @@
-extern crate libc;
 extern crate getopts;
 use std::io::Write;
+use std::os::raw::c_ulong;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let program = args[0].clone();
@@ -12,20 +13,48 @@ fn main() {
     opts.optflag("x", "hex", "hex output");
     let matches = opts.parse(&args[1..]).unwrap();
 
-    let random: [u8; 16] = [0; 16];
-
     if matches.opt_present("h") {
         println!("{}", opts.usage(&format!("Usage: {} [options]", program)));
-    } else if matches.opt_present("b") {
+        return;
+    }
+
+    let random = match auxv_random() {
+        None => { panic!("couldn't get auxv random") },
+        Some(x) => x,
+    };
+
+    if matches.opt_present("b") {
         std::io::stdout().write_all(&random[..]).unwrap();
     } else if matches.opt_present("x") {
         for x in random.iter() {
             print!("{:02x}", x);
         }
-        println!("")
+        println!("");
     } else {
-        for x in random.iter() {
-            print!("{} ", x);
+        for (i, x) in random.iter().enumerate() {
+            if i != 0 {
+                print!(" ")
+            }
+            print!("{}", x)
+        }
+        //let out: String = random.iter().map(|x| format!("{}", x)).collect::<Vec<_>>().join(" ");
+        //println!("{}", out);
+    }
+}
+
+extern "C" {
+    fn getauxval(ty: c_ulong) -> c_ulong;
+}
+
+static AT_RANDOM: c_ulong = 25;
+
+fn auxv_random() -> Option<&'static [u8; 16]> {
+    unsafe {
+        let r = getauxval(AT_RANDOM);
+        if r == 0 {
+            None
+        } else {
+            Some(std::mem::transmute(r))
         }
     }
 }
